c2aa219b8c2eeea43e30f8c8a7c5f0090bdb4571
brianberzins <brianberzins@gmail.com>
2022-07-18 13:58:14 -0500

    add unit test

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 	"net/http"
 )
 
+func greeting() string {
+	return "Hello"
+}
+
 func hello(responseWriter http.ResponseWriter, _ *http.Request) {
-	_, _ = fmt.Fprintf(responseWriter, "Welcome to the HomePage!")
+	_, _ = fmt.Fprintf(responseWriter, greeting())
 }
 
 func main() {
