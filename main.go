e97476a036d12bee152b772761d03b355c5e3f0b
brianberzins <brianberzins@gmail.com>
2022-07-18 13:48:17 -0500

    web server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(responseWriter http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprintf(responseWriter, "Welcome to the HomePage!")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/hello", hello)
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
