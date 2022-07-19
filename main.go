1e21a3a6c15cc28aae3655dc71f72e2f4b6486d7
brianberzins <brianberzins@gmail.com>
2022-07-19 11:55:45 -0500

    introduced path parameters

--- main.go
+++ main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func greeting(responseWriter http.ResponseWriter, _ *http.Request) {
-	_, _ = fmt.Fprintf(responseWriter, "hello")
+func greet(name string) string {
+	if name == "" {
+		return "hello"
+	}
+	return "hello " + name
 }
 
-func goodbye(responseWriter http.ResponseWriter, _ *http.Request) {
-	_, _ = fmt.Fprintf(responseWriter, "goodbye")
+func greeting(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, greet(mux.Vars(r)["name"]))
+}
+
+func goodbye(w http.ResponseWriter, _ *http.Request) {
+	_, _ = fmt.Fprintf(w, "goodbye")
+}
+
+func createRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello/{name}", greeting)
+	r.HandleFunc("/hello", greeting)
+	r.HandleFunc("/goodbye", goodbye)
+	return r
 }
 
 func main() {
-	http.HandleFunc("/hello", greeting)
-	http.HandleFunc("/goodbye", goodbye)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":8080", createRouter())
 }
--- main_test.go
+++ main_test.go
@@ -14,14 +14,16 @@ func TestGetRequests(t *testing.T) {
 		expectedCode int
 		expectedBody string
 	}{
-		{endpoint: "/hello", handler: greeting, expectedCode: http.StatusOK, expectedBody: "hello"},
-		{endpoint: "/goodbye", handler: goodbye, expectedCode: http.StatusOK, expectedBody: "goodbye"},
+		{endpoint: "/hello", expectedCode: http.StatusOK, expectedBody: "hello"},
+		{endpoint: "/goodbye", expectedCode: http.StatusOK, expectedBody: "goodbye"},
+		{endpoint: "/hello/murasaki", expectedCode: http.StatusOK, expectedBody: "hello murasaki"},
 	}
 
+	ro := createRouter()
 	for _, tc := range tests {
 		w := httptest.NewRecorder()
 		r := httptest.NewRequest(http.MethodGet, tc.endpoint, nil)
-		tc.handler(w, r)
+		ro.ServeHTTP(w, r)
 		assert.Equal(t, tc.expectedCode, w.Code)
 		assert.Equal(t, tc.expectedBody, w.Body.String())
 	}
