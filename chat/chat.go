a57e609a085d7dab9d5a4a32d1289bf630f4222f
brianberzins <brianberzins@gmail.com>
2022-07-26 10:27:57 -0500

    restructure

--- chat/chat.go
+++ chat/chat.go
@@ -1,14 +1,14 @@
-package main
+package chat
 
 import (
-	"fmt"
 	"golang.org/x/net/context"
+	"log"
 )
 
 type Server struct {
 }
 
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error) {
-	fmt.Printf("Received: %s", message.Body)
-	return &Message{Body: "Received on server"}, nil
+	log.Printf("Received: %s", message.Body)
+	return &Message{Body: "Hello from server"}, nil
 }
